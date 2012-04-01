0668e22dce45b08aa4dc38a37ee11448dfb730d5
Mike Covington <mfcovington@gmail.com>
2012-03-31 21:05:21 -0700

    update w/ 02.0.filtering_SNPs_by_pos_based_on_pepes.pl.local

--- 02.0.filtering_SNPs_by_pos.pl
+++ 02.0.filtering_SNPs_by_pos.pl
@@ -5,12 +5,14 @@ use Bio::DB::Sam;
 use Data::Dumper;
 use List::Util qw(max sum);
 
+my $ratio_threshold = 2 ##MFC
+
 #THIS SCRIPT REMOVES SNPS WITH LOW COVERAGE AND SUPORTED ONLY BY ON SEGMENT OF THE READS
 my %files = (
-		'homo' =>	{'snp_file' => "SNP_table.PEN.final.Picard_and_GATK.1.1.csv",
-					'out_file' => "02.2.SNP_table.PEN.final.Picard_and_GATK.1.1.filtered.csv"},
-		'het' =>	{'snp_file' => "SNP_table.PEN.final.Picard_and_GATK.hets.1.1.csv",
-					'out_file' => "02.2.SNP_table.PEN.final.Picard_and_GATK.hets.1.1.filtered.csv"},
+		'homo' =>	{'snp_file' => "01.2.SNP_table.PEN.final.Picard_and_GATK.1.1.csv.nogap.gap.csv",
+					'out_file' => "01.2.SNP_table.PEN.final.Picard_and_GATK.1.1.csv.nogap.gap.FILTERED.csv"},
+# 		'het' =>	{'snp_file' => "SNP_table.PEN.final.Picard_and_GATK.hets.1.1.csv",
+# 					'out_file' => "02.2.SNP_table.PEN.final.Picard_and_GATK.hets.1.1.filtered.csv"},
 			);
 
 foreach my $sp (keys %files){
@@ -29,7 +31,13 @@ foreach my $sp (keys %files){
 	chomp $head;
 	open(OUT, ">$out_file") or die "Can't open >$out_file for output: $!";
 	print OUT "$head\n";
-#	exit;
+	open(OUT_KEEP, ">$out_file.quint_keep.csv") or die "Can't open >$out_file.quint_keep.csv for output: $!";
+	open(OUT_KICK, ">$out_file.quint_kick.csv") or die "Can't open >$out_file.quint_kick.csv for output: $!";
+	print OUT_KEEP "$head,internal_terminal\n";
+	print OUT_KICK "$head\n";
+
+
+
 	
 	my ($counter,$counter_passed) = (0,0);
 	my %bases_n;
@@ -59,14 +67,33 @@ foreach my $sp (keys %files){
 				next;
 			}
 		}
+# 
+# 		#############################
+# 	#	#FILTER 2
+# 	#	#SKIP SNPs FOR WHICH ALL THE READS HAVE THE SNP IN THE SAME FIFTH (COUNTING FROM BOTH DIRECTIONS)
+# 		if($elements[15]!=1 && $elements[23]!=1){
+# 			print OUT join ",", @elements;
+# 			$counter_passed++;
+# #			print "close to juntion\t@elements" if($SNP_is_close_to_junction);
+# 		}
 
 		#############################
-	#	#FILTER 2
-	#	#SKIP SNPs FOR WHICH ALL THE READS HAVE THE SNP IN THE SAME FIFTH (COUNTING FROM BOTH DIRECTIONS)
-		if($elements[15]!=1 && $elements[23]!=1){
+	#	#FILTER 2 REDONE BY MFC
+	#	#SKIP SNPs FOR WHICH ALL THE READS HAVE THE SNP IN THE SAME "TERMINAL" FIFTH (COUNTING FROM BOTH DIRECTIONS) AND DON'T PASS FLANKING COVERAGE TEST
+		if($elements[15]!=1 && $elements[23]!=1){ ## Keep if the SNPs are not all in one fifth of the reads
 			print OUT join ",", @elements;
 			$counter_passed++;
-#			print "close to juntion\t@elements" if($SNP_is_close_to_junction);
+		} elsif (($elements[15]==1 && $elements[10]+$elements[11]+$elements[12] > 0) || ($elements[23]==1 && $elements[18]+$elements[19]+$elements[20] > 0)) { ## Keep if the SNPs are all in one fifth of the reads, but it is an internal quintile
+			print OUT_KEEP join ",", @elements, "internal";
+			print OUT join ",", @elements;
+			$counter_passed++;
+		} elsif (($elements[27]/$elements[26]>$ratio_threshold && $elements[30]/$elements[29]<$ratio_threshold) || ($elements[25]/$elements[26]>$ratio_threshold && $elements[28]/$elements[29]<$ratio_threshold)) { ## Kick if the SNPs are in a terminal quintile and don't pass the flanking coverage test
+			print OUT_KICK join ",", @elements;
+# 			$counter_passed++;
+		} else {
+			print OUT_KEEP join ",", @elements, "terminal";
+			print OUT join ",", @elements;
+			$counter_passed++;		
 		}
 
 	}
