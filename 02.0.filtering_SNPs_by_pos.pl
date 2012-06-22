62dbf1386622be8a5d0ce4dc1a66b0407662c96f
Mike Covington <mfcovington@gmail.com>
2012-06-22 16:21:06 -0700

    cleanup

--- 02.0.filtering_SNPs_by_pos.pl
+++ 02.0.filtering_SNPs_by_pos.pl
@@ -8,45 +8,40 @@
 use strict;
 use warnings;
 use autodie;
-# use Bio::DB::Sam;
-# use Data::Dumper;
 use List::Util qw( sum );
 use File::Basename;
 
-my $ratio_threshold = 2;
+my $ratio_threshold    = 2;
 my $coverage_threshold = 4;
-my ( $counter, $counter_passed );
 
 my @files = @ARGV;
+my ( $counter, $counter_passed );
 foreach my $file (@files) {
-	my $SNP_file = $file;
-    my ( $filename, $directories, $suffix ) = fileparse( $SNP_file, ".csv" );
-	my $out_file = "$directories$filename.FILTERED.csv";
+    my $snp_file = $file;
+    my ( $filename, $directories, $suffix ) = fileparse( $snp_file, ".csv" );
+    my $out_file = "$directories$filename.FILTERED.csv";
     my %bases_n;
-	
-    open SNPs, "<", $SNP_file;
-    open OUT,  ">", $out_file;
 
-    my $header = <SNPs>;
-    print OUT $header;
-	
-	while (<SNPs>){
-		$counter++;
+    open my $snp_fh, "<", $snp_file;
+    open my $out_fh, ">", $out_file;
+
+    my $header = <$snp_fh>;
+    print $out_fh $header;
+
+    while (<$snp_fh>){
+        $counter++;
         my @elements = split ",", $_;
-		my ( $scaff_name, $snp_pos, $reference, $snp_base ) = @elements[ 0 .. 2, 8 ];
+        my ( $scaff_name, $snp_pos, $reference, $snp_base ) = @elements[ 0 .. 2, 8 ];
 
-		#REMOVE THE DOT DECIMAL FROM THE INSERTIONS: 1001.1 => 1001
-		my $short_snp_pos = $snp_pos;
-		$short_snp_pos =~ s/\..*//;
-	
+        #REMOVE THE DOT DECIMAL FROM THE INSERTIONS: 1001.1 => 1001
+        my $short_snp_pos = $snp_pos;
+        $short_snp_pos =~ s/\..*//;
+
         # skip SNPs/indels supported by fewer than $coverage_threshold
-        if ( sum( @elements[ 3 .. 6 ] ) < $coverage_threshold    #ACGT
-            &&    $elements[7]          < $coverage_threshold    #deletion
-        ) {
-            next;
-        }
+        next if ( sum( @elements[ 3 .. 6 ] ) < $coverage_threshold       #ACGT
+                 &&    $elements[7]          < $coverage_threshold );    #deletion
 
-		# print SNPs/indels to output unless they don't pass the flanking coverage test
+        # print SNPs/indels to output unless they don't pass the flanking coverage test
         unless (   $elements[26] == 0                                        #avoid illegal division by zero
                 || (   $elements[27] / $elements[26] > $ratio_threshold
                     && $elements[30] / $elements[29] < $ratio_threshold )    #intron-exon junction
@@ -54,20 +49,21 @@ foreach my $file (@files) {
                     && $elements[28] / $elements[29] < $ratio_threshold )    #exon-intron junction
                )
         {
-            print OUT join ",", @elements;
+            print $out_fh join ",", @elements;
             $counter_passed++;
         }
 
-	}
-close OUT;
+    }
 
-my $counter_diff = $counter - $counter_passed;
-print "Filtered from $counter down to $counter_passed ("
-  . $counter_diff
-  . " SNPs/indels removed ("
-  . ( $counter - $counter_passed ) * 100 / $counter
-  . " %)) \n---Done!;\n";
+    close $snp_fh;
+    close $out_fh;
 
+    my $counter_diff = $counter - $counter_passed;
+    print "Filtered from $counter down to $counter_passed ("
+      . $counter_diff
+      . " SNPs/indels removed ("
+      . ( $counter - $counter_passed ) * 100 / $counter
+      . " %)) \n---Done!\n";
 }
-exit;
 
+exit;
