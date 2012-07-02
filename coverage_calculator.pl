72c5616daa3ef3e290c0d6be6b2f282e628049f2
Mike Covington <mfcovington@gmail.com>
2012-07-02 08:50:09 -0700

    /Volumes/Runner_3B/Mike_temp_SolRAID/Mike_SNPs/coverage_scripts/coverage_calculator.pl

--- coverage_calculator.pl
+++ coverage_calculator.pl
@@ -1,54 +1,35 @@
-#!/usr/bin/perl 
-
-use strict;
-use warnings;
+#!/usr/bin/perl
+# coverage_calculator.pl
+# Mike Covington
+# created: 2011-12-05
+#
+# Description: 
+#
+use strict; use warnings;
 use Data::Dumper;
 use Getopt::Long;
 use Bio::DB::Sam;
 use List::Util qw(sum);
 
-die "USAGE: FILE_NAME.pl </PATH/TO/file.bam> </PATH/TO/file.csv>\n" unless (@ARGV >= 1);
-my ($bam_file, @files) = @ARGV;
+###TODO: INCORPORATE GETOPT?
+
+die "USAGE: coverage_calculator.pl </PATH/TO/file.bam> </PATH/TO/DESTINATION/DIRECTORY> <col|whit> <m82|penn>\n" unless (@ARGV == 4);
+my ($bam_file, $out_dir, $machine, $species) = @ARGV;
 
 my $sam = Bio::DB::Sam->new(
 	-bam  => $bam_file,
 	-fasta => "/Volumes/SolexaRAID/Solexa_runs_Data/00.Downloaded_references_and_others/S_lycopersicum_chromosomes.2.40.fa"
 ); #my $sam
 
-open COVERAGE_OUT,	 ">coverage_files-col";
 my @chromosomes = $sam->seq_ids;
 foreach my $chr (@chromosomes) {
+	open (COVERAGE_OUT,	 ">$out_dir/$species.$chr.coverage.$machine") or die "Cannot open >$out_dir/$species.$chr.coverage.$machine";
 	$chr =~ tr/sl/SL/; # caps in chr names aren't consisent
 	my $cov_pos = 1;
 	my $chr_length = $sam->length($chr);
-	print $chr_length, "\n";
-# 	while ($cov_pos<$chr_length) {
-# 		$cov_pos++;
-# 		my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$cov_pos+999999);
-# 		my ($cov_val) = $coverage->coverage;
-# #		my @coveragedata = $coverage->coverage;
-# #		foreach my $cov_val (@coveragedata) {
-# 		print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
-# 		last if $cov_pos > 999999;
-# #		}
-# 	}
+	print "Getting $species coverage data for $chr (length = $chr_length)\n";
 
-
-# 	while ($cov_pos<$chr_length) {
-# 		if ($cov_pos+99999<$chr_length) {
-# 			$cov_pos++;
-# 			my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$cov_pos+99999);
-# 			my ($cov_val) = $coverage->coverage;
-# 			print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
-# 		} else {
-# 			$cov_pos++;
-# 			my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$chr_length);
-# 			my ($cov_val) = $coverage->coverage;
-# 			print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
-# 		}
-# 	}
-
-	my $chunk_size = 999999;  #99999 = 2 min and 16G still free; 999 is much longer (only 60% after 15 min) and worse on memory; 999999 = 2min and 23G left; 9999999 = 2 min 18G free
+	my $chunk_size = 999999;
 	while ($cov_pos<=$chr_length) {
 		if ($cov_pos+$chunk_size<$chr_length) {
 			my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$cov_pos+$chunk_size);
@@ -56,26 +37,17 @@ foreach my $chr (@chromosomes) {
 			foreach my $cov_val (@coveragedata) {
 				print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
 				$cov_pos++;
-			}
+			} #foreach @coveragedata
 		} else {
 			my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$chr_length);
 			my @coveragedata = $coverage->coverage;
 			foreach my $cov_val (@coveragedata) {
 				print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
 				$cov_pos++;
-			}
-		}
-	}
-
-
-# foreach my $cov_val (@coveragedata) {
-# 	print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
-# 	$cov_pos++;
-# }
-
-
+			} #foreach @coveragedata
+		} #ifelse
+	} #while ($cov_pos<=$chr_length)
+	close COVERAGE_OUT;
 }
-#print COVERAGE_OUT map { "$_ \n" } @coveragedata;
-close COVERAGE_OUT;
 exit;
 
