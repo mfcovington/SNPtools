ff3634abf351a7a91c8dfa00fbc6b818f65c6f4c
Mike Covington <mfcovington@gmail.com>
2012-07-02 09:24:57 -0700

    cleaned up and fixed potential 'defined' bugs

--- coverage_calculator.pl
+++ coverage_calculator.pl
@@ -3,77 +3,110 @@
 # Mike Covington
 # created: 2011-12-05
 #
-# Description: 
+# Description:
 #
-use strict; use warnings;
-use Data::Dumper;
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
 use Getopt::Long;
 use Bio::DB::Sam;
-use List::Util qw(sum);
 use IO::File;
 
-my ($ref_fa, $bam_file, $machine, $col, $whit, $id);
-my $out_dir = "./";
-my $usage = "\nUSAGE: coverage_calculator.v2.pl --ref </PATH/TO/reference.fa> --bam </PATH/TO/file.bam> --id <sample identifier for file.bam> --out </PATH/TO/DESTINATION/DIRECTORY/> --col OR --whit\n\n";
+my $usage = <<USAGE_END;
 
-my $options = GetOptions (
-	"ref=s"	=>	\$ref_fa,
-	"bam=s"	=>	\$bam_file,
-	"id=s"	=> \$id,
-	"out=s" =>	\$out_dir,
-	"col"	=> \$col,
-	"whit"	=> \$whit,
+USAGE:
+coverage_calculator.v2.pl
+  --ref </PATH/TO/reference.fa>
+  --bam </PATH/TO/file.bam>
+  --id <sample identifier for file.bam>
+  --out </PATH/TO/DESTINATION/DIRECTORY/>
+  --col OR --whit
+  --help
+
+USAGE_END
+
+my ( $ref_fa, $bam_file, $machine, $col, $whit, $id, $help );
+my $out_dir = "./";
+my $options = GetOptions(
+    "ref=s" => \$ref_fa,
+    "bam=s" => \$bam_file,
+    "id=s"  => \$id,
+    "out=s" => \$out_dir,
+    "col"   => \$col,
+    "whit"  => \$whit,
+    "help"  => \$help,
 );
 
-die $usage if defined ($col && $whit);
-die $usage unless defined ($ref_fa && $bam_file && $id);
+die $usage if $help;
+die $usage if defined $col && defined $whit;
+die $usage unless defined $ref_fa && defined $bam_file && defined $id;
 
 if ($col) {
-	$machine = "col";
-}elsif ($whit) {
-	$machine = "whit";
-}else{
-	die "Something is wrong...\n";
+    $machine = "col";
 }
-
-
+elsif ($whit) {
+    $machine = "whit";
+}
+else {
+    die "Something is wrong...\n";
+}
 
 my $sam = Bio::DB::Sam->new(
-	-bam  => $bam_file,
-	-fasta => $ref_fa
-); #my $sam
-
+    -bam   => $bam_file,
+    -fasta => $ref_fa
+);
 my @chromosomes = $sam->seq_ids;
-open (LOG, ">$out_dir/$id.$machine.log") or die "Cannot open >$out_dir/$id.$machine.log";
-LOG->autoflush(1);
-foreach my $chr (@chromosomes) {
-# 	$chr =~ tr/sl/SL/; # caps in chr names aren't consisent
-	open (COVERAGE_OUT,	 ">$out_dir/$id.$chr.coverage.$machine") or die "Cannot open >$out_dir/$id.$chr.coverage.$machine";
-	COVERAGE_OUT->autoflush(1);
-	my $cov_pos = 1;
-	my $chr_length = $sam->length($chr);
-	print LOG "Getting $id coverage data for $chr (length = $chr_length)\n";
 
-	my $chunk_size = 999999;
-	while ($cov_pos <= $chr_length) {
-		if ($cov_pos + $chunk_size < $chr_length) {
-			my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$cov_pos+$chunk_size);
-			my @coveragedata = $coverage->coverage;
-			foreach my $cov_val (@coveragedata) {
-				print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
-				$cov_pos++;
-			} #foreach @coveragedata
-		} else {
-			my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$chr_length);
-			my @coveragedata = $coverage->coverage;
-			foreach my $cov_val (@coveragedata) {
-				print COVERAGE_OUT "$chr,$cov_pos,$cov_val\n";
-				$cov_pos++;
-			} #foreach @coveragedata
-		} #ifelse
-	} #while ($cov_pos<=$chr_length)
-	close COVERAGE_OUT;
+open my $log_fh, ">", "$out_dir/$id.$machine.log";
+$log_fh->autoflush(1);
+
+foreach my $chr (@chromosomes) {
+
+    # $chr =~ tr/sl/SL/; # caps in chr names aren't consisent
+    open my $cov_out_fh, ">", "$out_dir/$id.$chr.coverage.$machine";
+    $cov_out_fh->autoflush(1);
+
+    my $cov_pos    = 1;
+    my $chr_length = $sam->length($chr);
+    say $log_fh "Getting $id coverage data for $chr (length = $chr_length)";
+
+    my $chunk_size = 999999;
+    while ( $cov_pos <= $chr_length ) {
+
+        if ( $cov_pos + $chunk_size < $chr_length ) {
+            my ($coverage) = $sam->features(
+                -type   => 'coverage',
+                -seq_id => $chr,
+                -start  => $cov_pos,
+                -end    => $cov_pos + $chunk_size
+            );
+            my @coveragedata = $coverage->coverage;
+
+            foreach my $cov_val (@coveragedata) {
+                say $cov_out_fh "$chr,$cov_pos,$cov_val";
+                $cov_pos++;
+            }
+
+        }
+        else {
+            my ($coverage) = $sam->features(
+                -type   => 'coverage',
+                -seq_id => $chr,
+                -start  => $cov_pos,
+                -end    => $chr_length
+            );
+            my @coveragedata = $coverage->coverage;
+
+            foreach my $cov_val (@coveragedata) {
+                say $cov_out_fh "$chr,$cov_pos,$cov_val";
+                $cov_pos++;
+            }
+
+        }
+    }
+    close $cov_out_fh;
 }
-close LOG;
+close $log_fh;
 exit;
 
