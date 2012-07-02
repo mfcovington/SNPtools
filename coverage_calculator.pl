08f64b11f0a06d5457e626af30c5a071998332b2
Mike Covington <mfcovington@gmail.com>
2012-07-02 08:55:56 -0700

    /Volumes/Runner_3B/Mike_temp_SolRAID/Mike_SNPs/coverage_scripts/coverage_calculator.v2.generic.pl

--- coverage_calculator.pl
+++ coverage_calculator.pl
@@ -11,37 +11,26 @@ use Getopt::Long;
 use Bio::DB::Sam;
 use List::Util qw(sum);
 
-###TODO: INCORPORATE GETOPT?
-
-# die "USAGE: coverage_calculator.pl </PATH/TO/file.bam> </PATH/TO/DESTINATION/DIRECTORY> <col|whit> <m82|penn>\n" unless (@ARGV == 4);
-my ($ref_fa, $bam_file, $out_dir, $machine, $col, $whit, $m82, $pen, $species);
-my $usage = "\nUSAGE: coverage_calculator.v2.pl --ref </PATH/TO/reference.fa> --bam </PATH/TO/file.bam> --out </PATH/TO/DESTINATION/DIRECTORY/> --col OR --whit --m82 OR --penn\n\n";
+my ($ref_fa, $bam_file, $machine, $col, $whit, $id);
+my $out_dir = "./";
+my $usage = "\nUSAGE: coverage_calculator.v2.pl --ref </PATH/TO/reference.fa> --bam </PATH/TO/file.bam> --id <sample identifier for file.bam> --out </PATH/TO/DESTINATION/DIRECTORY/> --col OR --whit\n\n";
 
 my $options = GetOptions (
 	"ref=s"	=>	\$ref_fa,
 	"bam=s"	=>	\$bam_file,
+	"id=s"	=> \$id,
 	"out=s" =>	\$out_dir,
 	"col"	=> \$col,
 	"whit"	=> \$whit,
-	"m82"	=> \$m82,
-	"pen"	=> \$pen
 );
 
-die $usage if defined $col && defined $whit;
-die $usage if defined $m82 && defined $pen;
+die $usage if defined ($col && $whit);
+die $usage unless defined ($ref_fa && $bam_file && $id);
 
 if ($col) {
 	$machine = "col";
 }elsif ($whit) {
 	$machine = "whit";
-}else{
-	die "Something is wrong...\n";
-}
-
-if ($m82) {
-	$species = "M82";
-}elsif ($pen) {
-	$species = "PEN";
 }else{
 	die "Something is wrong...\n";
 }
@@ -54,17 +43,17 @@ my $sam = Bio::DB::Sam->new(
 ); #my $sam
 
 my @chromosomes = $sam->seq_ids;
-open (LOG, ">$out_dir/$species.$machine.log") or die "Cannot open >$out_dir/$species.$machine.log";
-foreach my $chr (@chromosomes[7..12]) {
-	$chr =~ tr/sl/SL/; # caps in chr names aren't consisent
-	open (COVERAGE_OUT,	 ">$out_dir/$species.$chr.coverage.$machine") or die "Cannot open >$out_dir/$species.$chr.coverage.$machine";
+open (LOG, ">$out_dir/$id.$machine.log") or die "Cannot open >$out_dir/$id.$machine.log";
+foreach my $chr (@chromosomes) {
+# 	$chr =~ tr/sl/SL/; # caps in chr names aren't consisent
+	open (COVERAGE_OUT,	 ">$out_dir/$id.$chr.coverage.$machine") or die "Cannot open >$out_dir/$id.$chr.coverage.$machine";
 	my $cov_pos = 1;
 	my $chr_length = $sam->length($chr);
-	print LOG "Getting $species coverage data for $chr (length = $chr_length)\n";
+	print LOG "Getting $id coverage data for $chr (length = $chr_length)\n";
 
 	my $chunk_size = 999999;
-	while ($cov_pos<=$chr_length) {
-		if ($cov_pos+$chunk_size<$chr_length) {
+	while ($cov_pos <= $chr_length) {
+		if ($cov_pos + $chunk_size < $chr_length) {
 			my ($coverage) = $sam->features(-type=>'coverage',-seq_id=>$chr,-start=>$cov_pos,-end=>$cov_pos+$chunk_size);
 			my @coveragedata = $coverage->coverage;
 			foreach my $cov_val (@coveragedata) {
