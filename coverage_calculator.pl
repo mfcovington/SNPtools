1423fa58c1aae78146d36552c8a961701fc3a44d
Mike Covington <mfcovington@gmail.com>
2012-07-03 09:22:47 -0700

    altered orig cov calc script to use coverage_commander

--- coverage_calculator.pl
+++ coverage_calculator.pl
@@ -12,6 +12,7 @@ use feature 'say';
 use Getopt::Long;
 use Bio::DB::Sam;
 use IO::File;
+use coverage_commander;
 
 my $usage = <<USAGE_END;
 
@@ -19,94 +20,45 @@ USAGE:
 coverage_calculator.v2.pl
   --ref </PATH/TO/reference.fa>
   --bam </PATH/TO/file.bam>
-  --id <sample identifier for file.bam>
+  --id  <sample identifier for file.bam>
   --out </PATH/TO/DESTINATION/DIRECTORY/>
-  --col OR --whit
   --help
 
 USAGE_END
 
-my ( $ref_fa, $bam_file, $machine, $col, $whit, $id, $help );
+my ( $bam_file, $id, $help );
 my $out_dir = "./";
 my $options = GetOptions(
-    "ref=s" => \$ref_fa,
     "bam=s" => \$bam_file,
     "id=s"  => \$id,
     "out=s" => \$out_dir,
-    "col"   => \$col,
-    "whit"  => \$whit,
     "help"  => \$help,
 );
 
 die $usage if $help;
-die $usage if defined $col && defined $whit;
-die $usage unless defined $ref_fa && defined $bam_file && defined $id;
-
-if ($col) {
-    $machine = "col";
-}
-elsif ($whit) {
-    $machine = "whit";
-}
-else {
-    die "Something is wrong...\n";
-}
+die $usage unless defined $bam_file && defined $id;
 
 my $sam = Bio::DB::Sam->new(
     -bam   => $bam_file,
-    -fasta => $ref_fa
+    # -fasta => $ref_fa
 );
 my @chromosomes = $sam->seq_ids;
 
-open my $log_fh, ">", "$out_dir/$id.$machine.log";
-$log_fh->autoflush(1);
+# open my $log_fh, ">", "$out_dir/$id.log";
+# $log_fh->autoflush(1);
+
+my $coverage = coverage_commander->new(
+    bam     => "/Users/mfc/sandbox/genotyping/bwa_tophat_M82-Slyc.sorted.dupl_rm.bam",
+    verbose => 1,
+    # log     => "$out_dir/$id.log",
+);
 
 foreach my $chr (@chromosomes) {
-
-    # $chr =~ tr/sl/SL/; # caps in chr names aren't consisent
-    open my $cov_out_fh, ">", "$out_dir/$id.$chr.coverage.$machine";
-    $cov_out_fh->autoflush(1);
-
-    my $cov_pos    = 1;
-    my $chr_length = $sam->length($chr);
-    say $log_fh "Getting $id coverage data for $chr (length = $chr_length)";
-
-    my $chunk_size = 999999;
-    while ( $cov_pos <= $chr_length ) {
-
-        if ( $cov_pos + $chunk_size < $chr_length ) {
-            my ($coverage) = $sam->features(
-                -type   => 'coverage',
-                -seq_id => $chr,
-                -start  => $cov_pos,
-                -end    => $cov_pos + $chunk_size
-            );
-            my @coveragedata = $coverage->coverage;
-
-            foreach my $cov_val (@coveragedata) {
-                say $cov_out_fh "$chr,$cov_pos,$cov_val";
-                $cov_pos++;
-            }
-
-        }
-        else {
-            my ($coverage) = $sam->features(
-                -type   => 'coverage',
-                -seq_id => $chr,
-                -start  => $cov_pos,
-                -end    => $chr_length
-            );
-            my @coveragedata = $coverage->coverage;
-
-            foreach my $cov_val (@coveragedata) {
-                say $cov_out_fh "$chr,$cov_pos,$cov_val";
-                $cov_pos++;
-            }
-
-        }
-    }
-    close $cov_out_fh;
+    my $cov_out = "$out_dir/$id.$chr.coverage";
+    $coverage->{chromosome} = $chr;
+    $coverage->{out_file}   = $cov_out;
+    $coverage->get_coverage();
 }
-close $log_fh;
+# close $log_fh;
 exit;
 
