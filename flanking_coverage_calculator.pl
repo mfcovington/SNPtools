f394bec1fa9797c1439a21984666d492792015af
Mike Covington <mfcovington@gmail.com>
2012-07-13 23:03:58 -0700

    cleanup and initial updating

--- flanking_coverage_calculator.pl
+++ flanking_coverage_calculator.pl
@@ -1,38 +1,63 @@
-#!/usr/bin/perl
-# flanking_coverage_calculator_2.pl
+#!/usr/bin/env perl
+# flanking_coverage_calculator.pl
 # Mike Covington
 # created: 2011-12-11
 #
 # Description: 
 #
-use strict; use warnings;
+use strict;
+use warnings;
+use autodie;
 use File::Basename;
 
+# new version of coverage data is tab-delimited and only has data for regions with coverage
 
-die "\n\tUSAGE: flanking_coverage_calculator_2.pl <SNP.CSV> <COL.COV> <WHIT.COV>\n\n" unless (@ARGV == 3);
-my ($snp_in, $COL_in, $WHIT_in) = @ARGV;
-#open files
-open (SNP_IN, $snp_in) or die "Can't open $snp_in";
-open (COL_COV_IN, $COL_in) or die "Can't open $COL_in";
-open (WHIT_COV_IN, $WHIT_in) or die "Can't open $WHIT_in";
+my $usage = <<USAGE_END;
+
+USAGE:
+flanking_coverage_calculator.pl
+  --snp_file   </PATH/TO/snp_file.csv>
+  --cov_prefix </PATH/TO/cov_file_prefix_only -- w/o .cov_(no)gap>
+  --help
+
+USAGE_END
+
+my ( $snp_in, $cov_prefix, $help );
+my $out_dir = "./";
+my $options = GetOptions(
+    "snp_file=s"   => \$snp_in,
+    "cov_prefix=s" => \$cov_prefix,
+    "help"         => \$help,
+);
+
+die $usage if $help;
+die $usage unless defined $snp_in && defined $cov_prefix;
+
+my ( $cov_gaps, $cov_nogaps ) = ( $cov_prefix, $cov_prefix );
+$cov_gaps .= ".cov_gaps";
+$cov_nogaps .= ".cov_nogaps";
 my ($filename, $directories, $suffix) = fileparse($snp_in, ".csv");
 my $snp_out = $directories . $filename . ".nogap.gap.csv";
-open (SNP_OUT, ">$snp_out") or die "Can't open $snp_out";
+#open files
+open $snp_in_fh,     "<", $snp_in;
+open $snp_out_fh,    ">", $snp_out;
+open $cov_nogaps_fh, "<", $cov_nogaps;
+open $cov_gaps_fh,   "<", $cov_gaps;
 
 #read in first 17 lines
 my (@COL_cov_chr, @COL_cov_pos, @COL_cov, @WHIT_cov_chr, @WHIT_cov_pos, @WHIT_cov, $COL_line, $WHIT_line);
 for (my $count = 0; $count < 17; $count++) {
-	$COL_line = <COL_COV_IN>;
-	$WHIT_line = <WHIT_COV_IN>;
+	$COL_line = <$cov_nogaps_fh>;
+	$WHIT_line = <$cov_gaps_fh>;
 	chomp ($COL_line, $WHIT_line);
  	($COL_cov_chr[$count], $COL_cov_pos[$count], $COL_cov[$count]) = split(/,/, $COL_line);
  	($WHIT_cov_chr[$count], $WHIT_cov_pos[$count], $WHIT_cov[$count]) = split(/,/, $WHIT_line);
 }
 
-my $header = <SNP_IN>;
+my $header = <$snp_in_fh>;
 chomp $header;
-print SNP_OUT join(",",$header, "nogap_cov_pos-8","nogap_cov_pos","nogap_cov_pos+8", "gap_cov_pos-8","gap_cov_pos","gap_cov_pos+8"), "\n";
-while (my $snp_line = <SNP_IN>) {
+print $snp_out_fh join(",",$header, "nogap_cov_pos-8","nogap_cov_pos","nogap_cov_pos+8", "gap_cov_pos-8","gap_cov_pos","gap_cov_pos+8"), "\n";
+while (my $snp_line = <$snp_in_fh>) {
 	chomp $snp_line;
 	my ($snp_chr, $snp_pos_unsplit, $snp_remainder) = split(/,/, $snp_line, 3); #read in CSV line
 	my ($snp_pos, $snp_pos_index) = split(/\./, $snp_pos_unsplit);
@@ -43,8 +68,8 @@ while (my $snp_line = <SNP_IN>) {
 		shift @WHIT_cov_chr;
 		shift @WHIT_cov_pos;
 		shift @WHIT_cov;
-		$COL_line = <COL_COV_IN>;
-		$WHIT_line = <WHIT_COV_IN>;
+		$COL_line = <$cov_nogaps_fh>;
+		$WHIT_line = <$cov_gaps_fh>;
 		chomp ($COL_line, $WHIT_line);
  		my @COL_line_elements = split(/,/, $COL_line);
  		my @WHIT_line_elements = split(/,/, $WHIT_line);
@@ -56,15 +81,15 @@ while (my $snp_line = <SNP_IN>) {
 		push @WHIT_cov, $WHIT_line_elements[2];
 	}
 	if ($snp_chr eq $COL_cov_chr[8] && $snp_pos == $COL_cov_pos[8]) {
-		print SNP_OUT join (",", $snp_chr, $snp_pos_unsplit, $snp_remainder, $COL_cov[0], $COL_cov[8], $COL_cov[16], $WHIT_cov[0], $WHIT_cov[8], $WHIT_cov[16]), "\n";
+		print $snp_out_fh join (",", $snp_chr, $snp_pos_unsplit, $snp_remainder, $COL_cov[0], $COL_cov[8], $COL_cov[16], $WHIT_cov[0], $WHIT_cov[8], $WHIT_cov[16]), "\n";
 	}else{
 		die "Something strange going on here...";
 	}
 }
 
-close SNP_IN;
-close COL_COV_IN;
-close WHIT_COV_IN;
-close SNP_OUT;
+close $snp_in_fh;
+close $cov_nogaps_fh;
+close $cov_gaps_fh;
+close $snp_out_fh;
 
 
