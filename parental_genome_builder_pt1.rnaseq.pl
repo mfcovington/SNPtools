84fb8edd784d9acb158e7f36c265ba92686dbbe0
Mike Covington <mfcovington@gmail.com>
2012-07-15 14:49:31 -0700

    changed chr# to chr_id; specify files, not directories; File::Path; fixed min_cov; cleanup

--- parental_genome_builder_pt1.rnaseq.pl
+++ parental_genome_builder_pt1.rnaseq.pl
@@ -5,54 +5,46 @@
 #
 # Description: 
 #
-# Change Log:
-#	2012-01-15: adapted for use with RNAseq data only (SNPs + indels)
-#	2012-02-23: Added wildcard to and altered structure of SNP files to account for difference caused by changing upstream scripts
-#	2012-02-23: Changed capitalization of coverage files to account for difference caused by changing upstream scripts
-#	2012-02-23: Changed `mkdir $output_dir` to `mkdir -p $output_dir`
-#
 use strict;
 use warnings;
 use autodie;
 use feature 'say';
 use Getopt::Long;
+use File::Path 'make_path';
 
 # TODO:
-    # transition from chr # to chr ID
-    # make write_to_master_snp subroutine
+    # make write_master_snp subroutine
 
 my $usage = <<USAGE_END;
 
-USAGE:                                            #this usage statement doesn't reflect reality, yet
+USAGE:
 parental_genome_builder_pt1.rnaseq.pl
-  --chr     <chromosome number>                   #need to change this to chr id eventually!!
-  --snp1    <parent #1 snp file>                  #currently, everything is pointing to directories
-  --snp2    <parent #2 snp file>
-  --cov1    <parent #1 coverage.cov_nogap file>
-  --cov2    <parent #2 coverage.cov_nogap file>
-  --name1   <parent #1 name>
-  --name2   <parent #2 name>
-  --out     <output directory>
-  --min_cov <minimum coverage [4]>
-  --help
+  --chr_id     chromosome ID
+  --snp1       parent #1 snp file
+  --snp2       parent #2 snp file
+  --cov1       parent #1 coverage.cov_nogap file
+  --cov2       parent #2 coverage.cov_nogap file
+  --name1      parent #1 name
+  --name2      parent #2 name
+  --out        output directory
+  --min_cov    minimum coverage [4]
+  --help       (displays this usage statement)
 
 USAGE_END
 
-#defaults
-my $par1_snp_dir = "/Volumes/Runner_3B/Mike_temp_SolRAID/Mike_SNPs/M82_SNPS";
-my $par2_snp_dir = "/Volumes/Runner_3B/Mike_temp_SolRAID/Mike_SNPs/PENN_SNPS";
-my $cov_dir      = "/Volumes/Runner_3B/Mike_temp_SolRAID/Mike_SNPs/coverage_out";
-my $output_dir   = "./";
-my $par1_name    = "PAR1"
-my $par1_name    = "PAR2"
-my $min_cov      = 4;
-my ( $chr_num, $help );
+# defaults/options
+my $output_dir = "./";
+my $par1_name  = "PAR1";
+my $par1_name  = "PAR2";
+my $min_cov    = 4;
+my ( $chr_id, $par1_snp_file, $par2_snp_file, $par1_cov_file, $par2_cov_file, $help );
 
 GetOptions(
-    "chr=i"     => \$chr_num,
-    "msnp=s"    => \$par1_snp_dir,
-    "psnp=s"    => \$par2_snp_dir,
-    "cov=s"     => \$cov_dir,
+    "chr_id=s"  => \$chr_id,
+    "snp1=s"    => \$par1_snp_file,
+    "snp2=s"    => \$par2_snp_dir,
+    "cov1=s"    => \$par1_cov_file,
+    "cov2=s"    => \$par1_cov_file,
     "name1=s"   => \$par1_name,
     "name2=s"   => \$par2_name,
     "out=s"     => \$output_dir,
@@ -61,30 +53,18 @@ GetOptions(
 );
 
 die $usage if $help;
-die $usage unless defined $chr_num;
-
-my $chr_offset = $chr_num + 1;
-my $chr_format;
-if ( $chr_num < 10 ) {
-    $chr_format = "0$chr_num";
-}
-else {
-    $chr_format = $chr_num;
-}
-
-# input files
-my $par1_snps = glob($par1_snp_dir . "/01.2.SNP_table.bwa_tophat_*.sorted.dupl_rm.$chr_offset.$chr_offset.nogap.gap.FILTERED.csv");
-my $par2_snps = glob($par2_snp_dir . "/01.2.SNP_table.bwa_tophat_*.sorted.dupl_rm.$chr_offset.$chr_offset.nogap.gap.FILTERED.csv");
-my $par1_cov_file = $cov_dir . "/M82.SL2.40ch$chr_format.coverage.col";
-my $par2_cov_file = $cov_dir . "/PEN.SL2.40ch$chr_format.coverage.col";
-open $par1_cov_fh, "<", $par1_cov_file;
-open $par2_cov_fh, "<", $par2_cov_file;
+die $usage
+  unless defined $chr_id
+      && defined $par1_snp_file
+      && defined $par2_snp_file
+      && defined $par1_cov_file
+      && defined $par2_cov_file;
 
 # snp/cov build hashes
-my %par1_snps = snp_hash_builder( $par1_snps );
-my %par2_snps = snp_hash_builder( $par2_snps );
-my %par1_cov  = cov_hash_builder( $par1_cov_file );
-my %par2_cov  = cov_hash_builder( $par2_cov_file );
+my %par1_snps = snp_hash_builder($par1_snps_file);
+my %par2_snps = snp_hash_builder($par2_snps_file);
+my %par1_cov  = cov_hash_builder($par1_cov_file);
+my %par2_cov  = cov_hash_builder($par2_cov_file);
 
 # get snp positions with good coverage
 my @all_snp_pos = sort { $a <=> $b } keys %{ { %par1_snps, %par2_snps } };
@@ -92,44 +72,44 @@ my @good_cov_pos;
 for my $pos (@all_snp_pos) {
     my $pos_par1_cov = $par1_cov{$pos} || next;    # || next bypasses if undef
     my $pos_par2_cov = $par2_cov{$pos} || next;
-    next unless $pos_par1_cov > 4 && $pos_par1_cov > 4;
+    next if $pos_par1_cov < $min_cov || $pos_par1_cov < $min_cov;
     push @good_cov_pos, $pos;
 }
 
 # write master snp file
-`mkdir -p $output_dir`;
-my $master_snp_file = "$output_dir/master_snp_list_rnaseq.chr$chr_format";
+make_path($output_dir);
+my $master_snp_file = "$output_dir/master_snp_list.$par1_name.vs.$par2_name.$chr_id";
 open $master_fh, ">", $master_snp_file;
-say $master_fh join "\t", "chr", "pos", "ref_base", "snp_base", "genotype", "insert_position";    # insert_position will be decimal portion of position for insertions
+say $master_fh join "\t", "chr", "pos", "ref_base", "snp_base", "genotype", "insert_position";
 
 for my $pos (@good_cov_pos) {
     if ( defined $par1_snps{$pos} ) {
         my $insert_length = $par1_snps{$pos}{insert} || 0;
-        if ( $insert_length ) {
-            for my $insert_idx ( 1 .. $insert_length ){
+        if ($insert_length) {
+            for my $insert_idx ( 1 .. $insert_length ) {
                 $insert_idx = "0$insert_idx" if $insert_idx < 10;
-                say $master_fh join "\t", "insert chr here", $pos, @{ $par1_snps{$pos}{$insert_idx} }[0,1], $par1_name, $insert_idx;
+                say $master_fh join "\t", "insert chr here", $pos, @{ $par1_snps{$pos}{$insert_idx} }[ 0, 1 ], $par1_name, $insert_idx;
             }
         }
         else{
-            say $master_fh join "\t", "insert chr here", $pos, @{ $par1_snps{$pos}{snp_del} }[0,1], $par1_name, "NA";
+            say $master_fh join "\t", "insert chr here", $pos, @{ $par1_snps{$pos}{snp_del} }[ 0, 1 ], $par1_name, "NA";
         }
     }
     if ( defined $par2_snps{$pos} ) {
         my $insert_length = $par2_snps{$pos}{insert} || 0;
-        if ( $insert_length ) {
-            for my $insert_idx ( 1 .. $insert_length ){
+        if ($insert_length) {
+            for my $insert_idx ( 1 .. $insert_length ) {
                 $insert_idx = "0$insert_idx" if $insert_idx < 10;
-                say $master_fh join "\t", "insert chr here", $pos, @{ $par2_snps{$pos}{$insert_idx} }[0,1], $par2_name, $insert_idx;
+                say $master_fh join "\t", "insert chr here", $pos, @{ $par2_snps{$pos}{$insert_idx} }[ 0, 1 ], $par2_name, $insert_idx;
             }
         }
         else{
-            say $master_fh join "\t", "insert chr here", $pos, @{ $par2_snps{$pos}{snp_del} }[0,1], $par2_name, "NA";
+            say $master_fh join "\t", "insert chr here", $pos, @{ $par2_snps{$pos}{snp_del} }[ 0, 1 ], $par2_name, "NA";
         }
     }
 }
 
-close ($master_fh);
+close $master_fh;
 exit;
 
 ###############
@@ -141,7 +121,7 @@ sub snp_hash_builder {
     open my $par_snps_fh, "<", $par_snps_file;
     my $header = <$par_snps_fh>;    # do I care about this?
     my %par_snps;
-    while ( <$par_snps_fh> ) {
+    while (<$par_snps_fh>) {
         chomp;
         my @par_snp = split /,/;
         my @par_pos = split /\./, $par_snp[1];
@@ -150,7 +130,7 @@ sub snp_hash_builder {
             $par_snps{$par_pos[0]}{insert}      = $par_pos[1];
         }
         else {
-            $par_snps{$par_pos[0]}{snp_del} = [ @par_snp[ 2, 8 ] ];
+            $par_snps{ $par_pos[0] }{snp_del} = [ @par_snp[ 2, 8 ] ];
         }
     }
     close $par_snps_fh;
