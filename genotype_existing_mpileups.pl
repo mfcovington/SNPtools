5588cd573d7307272017c426af08208f7a472bd4
Mike Covington <mfcovington@gmail.com>
2012-11-28 18:03:39 -0800

    re-introduced bam/fasta requirement to genotype_existing_mpileups.pl

--- genotype_existing_mpileups.pl
+++ genotype_existing_mpileups.pl
@@ -18,6 +18,8 @@ snp_finder.pl
   --id           Sample identifier
   --par1         Parent 1 ID
   --par2         Parent 2 ID
+  --bam          Sample alignment file (.bam)
+  --fasta        Reference file (.fasta/.fa)
   --out_dir      Output directory [current]
   --threads      Number of threads [1]
   --verbose
@@ -25,11 +27,13 @@ snp_finder.pl
 
 USAGE_END
 
-my ( $id, $par1, $par2, $out_dir, $threads, $verbose, $help );
+my ( $id, $par1, $par2, $bam_file, $fasta_file, $out_dir, $threads, $verbose, $help );
 my $options = GetOptions(
     "id=s"      => \$id,
     "par1=s"    => \$par1,
     "par2=s"    => \$par2,
+    "bam=s"     => \$bam_file,
+    "fasta=s"   => \$fasta_file,
     "out_dir=s" => \$out_dir,
     "threads=i" => \$threads,
     "verbose"   => \$verbose,
@@ -40,11 +44,15 @@ die $usage unless $options;
 die $usage if $help;
 die $usage
   unless defined $id
+  && defined $bam_file
+  && defined $fasta_file;
 
 my $geno = genotyping_commander->new(
     id      => $id,
     par1    => $par1,
     par2    => $par2,
+    bam     => $bam_file,
+    fasta   => $fasta_file,
     out_dir => $out_dir,
     threads => $threads,
     verbose => $verbose,
