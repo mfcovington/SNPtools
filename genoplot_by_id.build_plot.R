a2d4823ec3933f3d586770540ddff7b25d6a8c88
Mike Covington <mfcovington@gmail.com>
2012-12-15 20:35:24 -0800

    change y-axis labels: lab = %/% 10 * 10 for max and min, independently

--- genoplot_by_id.build_plot.R
+++ genoplot_by_id.build_plot.R
@@ -1,21 +1,20 @@
 library("ggplot2")
 
-max_cov <- max(geno_df$cov)
 offset <- 1.05
-ylimits <- max_cov * offset
 
 # added due to broken feature in ggplot 0.9.1:
 # is it still broken/required?
-max_pos  <- max(geno_df$pos)
+max_pos <- max(geno_df$pos)
 temp_max <- floor(max_pos / 10000000)
 max_pos_fixed <- temp_max * 10000000
 label_max <- temp_max * 10
 
-max <- max(geno_df$cov.plot)
-min <- min(geno_df$cov.plot)
-max_lab <-  (max %/% 10    ) * 10
-min_lab <- -(min %/% 10 + 1) * 10 # make sure min < 0?
-min.min <- min(max_lab, min_lab)
+max <-  max(geno_df$cov.plot)
+min <- -min(geno_df$cov.plot)
+if(max < 0) max <- 0
+if(min < 0) min <- 0
+max_lab <- (max %/% 10) * 10
+min_lab <- (min %/% 10) * 10
 
 geno.plot <- ggplot(geno_df, aes(pos, cov.plot)) +
   geom_point(size = 1, aes(color = (par2 - par1) / cov)) +
@@ -40,9 +39,9 @@ geno.plot <- ggplot(geno_df, aes(pos, cov.plot)) +
   ) +
   scale_y_continuous(
     'Coverage',
-    breaks = c(-min.min, 0, min.min),
-    labels = c( min.min, 0, min.min),
-    limits = c(offset*min, offset*max)
+    breaks = c(-min_lab, 0, max_lab),
+    labels = c( min_lab, 0, max_lab),
+    limits = c(-offset*min, offset*max)
   )
 
 
