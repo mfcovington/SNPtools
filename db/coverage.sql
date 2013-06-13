aeef49b7a2e16e3f563aceba0bdf94e9719dfe68
Mike Covington <mfcovington@gmail.com>
2013-06-13 16:24:49 -0700

    add sample_id to composite key

--- db/coverage.sql
+++ db/coverage.sql
@@ -1,7 +1,8 @@
 CREATE TABLE coverage (
-    chromosome  TEXT    NOT NULL,
-    position    INTEGER NOT NULL,
-    gap_depth   INTEGER NOT NULL,
-    nogap_depth INTEGER NOT NULL,
-    PRIMARY KEY (chromosome, position)
+    sample_id  TEXT    NOT NULL,
+    chromosome TEXT    NOT NULL,
+    position   INTEGER NOT NULL,
+    gap_cov    INTEGER NOT NULL,
+    nogap_cov  INTEGER NOT NULL,
+    PRIMARY KEY ( sample_id, chromosome, position )
 );
