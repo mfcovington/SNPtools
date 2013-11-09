6025ceffe28e22c1f50b279324143493287cc7ef
Mike Covington <mfcovington@gmail.com>
2013-11-08 19:03:56 -0800

    Change structure of SNP hash (pos-id-polymorphism)

--- bin/SNPfinder/parental_genome_builder_pt1.rnaseq.pl
+++ bin/SNPfinder/parental_genome_builder_pt1.rnaseq.pl
@@ -62,10 +62,10 @@ die $usage
       && defined $par2_cov_file;
 
 # snp/cov build hashes
-my %par1_snps = snp_hash_builder($par1_snp_file);
-my %par2_snps = snp_hash_builder($par2_snp_file);
 my %par1_cov  = cov_hash_builder($par1_cov_file);
 my %par2_cov  = cov_hash_builder($par2_cov_file);
+my %par1_snps = snp_hash_builder( $par1_name, $par1_snp_file);
+my %par2_snps = snp_hash_builder( $par2_name, $par2_snp_file);
 
 # get snp positions with good coverage
 my @all_snp_pos = sort { $a <=> $b } keys %{ { %par1_snps, %par2_snps } };
@@ -120,7 +120,7 @@ exit;
 ###############
 
 sub snp_hash_builder {
-    my $par_snps_file = shift;
+    my ( $par_id, $par_snps_file ) = @_;
     open my $par_snps_fh, "<", $par_snps_file;
     my $header = <$par_snps_fh>;    # do I care about this?
     my %par_snps;
@@ -129,11 +129,11 @@ sub snp_hash_builder {
         my @par_snp = split /,/;
         my @par_pos = split /\./, $par_snp[1];
         if ( defined $par_pos[1] ) {
-            $par_snps{$par_pos[0]}{$par_pos[1]} = [ @par_snp[ 2, 8 ], $par_pos[1] ];
-            $par_snps{$par_pos[0]}{insert}      = $par_pos[1];
+            $par_snps{$par_pos[0]}{$par_id}{$par_pos[1]} = [ @par_snp[ 2, 8 ], $par_pos[1] ];
+            $par_snps{$par_pos[0]}{$par_id}{insert}      = $par_pos[1];
         }
         else {
-            $par_snps{ $par_pos[0] }{snp_del} = [ @par_snp[ 2, 8 ] ];
+            $par_snps{ $par_pos[0] }{$par_id}{snp_del} = [ @par_snp[ 2, 8 ] ];
         }
     }
     close $par_snps_fh;
