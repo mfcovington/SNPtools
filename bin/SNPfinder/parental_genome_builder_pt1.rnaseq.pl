2b47505a1063fb68f83d6c4c7c6ada88a40d1f8b
Mike Covington <mfcovington@gmail.com>
2013-11-10 02:24:03 -0800

    Add TODOs; Cleanup

--- bin/SNPfinder/parental_genome_builder_pt1.rnaseq.pl
+++ bin/SNPfinder/parental_genome_builder_pt1.rnaseq.pl
@@ -14,6 +14,8 @@ use File::Path 'make_path';
 use SNPtools::Coverage::DB::Main;
 
 # TODO: Make write_master_snp subroutine
+# TODO: Incorporate into module
+# TODO: Run in parallel by chromosome (test memory load)
 
 my $usage = <<USAGE_END;
 
@@ -22,8 +24,6 @@ parental_genome_builder_pt1.rnaseq.pl
   --chr        chromosome ID
   --snp1       parent #1 snp file
   --snp2       parent #2 snp file
-  --cov1       parent #1 coverage.cov_nogap file
-  --cov2       parent #2 coverage.cov_nogap file
   --par1       parent #1 name
   --par2       parent #2 name
   --out_dir    output directory
@@ -37,16 +37,14 @@ my $out_dir = "./";
 my $par1    = "PAR1";
 my $par2    = "PAR2";
 my $min_cov = 4;
-my ( $chr, $par1_snp_file, $par2_snp_file, $par1_cov_file, $par2_cov_file, $help );
+my ( $chr, $par1_snp_file, $par2_snp_file, $help );
 
 GetOptions(
     "chr=s"     => \$chr,
     "snp1=s"    => \$par1_snp_file,
     "snp2=s"    => \$par2_snp_file,
-    "cov1=s"    => \$par1_cov_file,
-    "cov2=s"    => \$par2_cov_file,
-    "name1=s"   => \$par1,
-    "name2=s"   => \$par2,
+    "par1=s"    => \$par1,
+    "par2=s"    => \$par2,
     "out_dir=s" => \$out_dir,
     "min_cov=i" => \$min_cov,
     "help"      => \$help,
@@ -56,9 +54,7 @@ die $usage if $help;
 die $usage
   unless defined $chr
       && defined $par1_snp_file
-      && defined $par2_snp_file
-      && defined $par1_cov_file
-      && defined $par2_cov_file;
+      && defined $par2_snp_file;
 
 my %snps;
 snp_hash_builder( $par1, $par1_snp_file, \%snps);
@@ -90,7 +86,8 @@ my $master_snp_dir = "$out_dir/master_snp_lists";
 make_path($master_snp_dir);
 my $master_snp_file = "$master_snp_dir/master_snp_list.$par1.vs.$par2.$chr";
 open my $master_fh, ">", $master_snp_file;
-say $master_fh join "\t", "chr", "pos", "ref_base", "snp_base", "genotype", "insert_position";
+say $master_fh join "\t", "chr", "pos", "ref_base", "snp_base", "genotype",
+  "insert_position";
 
 for my $pos (@good_cov_pos) {
 
@@ -103,12 +100,18 @@ for my $pos (@good_cov_pos) {
         if ($insert_length) {
             for my $insert_idx ( 1 .. $insert_length ) {
                 $insert_idx = "0$insert_idx" if $insert_idx < 10;
-                next unless defined $snps{$pos}{$par_id}{$insert_idx}[2];    # there are occasional gaps due to insufficient coverage
-                say $master_fh join "\t", $chr, $pos, @{ $snps{$pos}{$par_id}{$insert_idx} }[ 0, 1 ], $par_id, $insert_idx;
+
+                # there are occasional gaps due to insufficient coverage
+                next unless defined $snps{$pos}{$par_id}{$insert_idx}[2];
+
+                say $master_fh join "\t", $chr, $pos,
+                  @{ $snps{$pos}{$par_id}{$insert_idx} }[ 0, 1 ], $par_id,
+                  $insert_idx;
             }
         }
         else{
-            say $master_fh join "\t", $chr, $pos, @{ $snps{$pos}{$par_id}{snp_del} }[ 0, 1 ], $par_id, "NA";
+            say $master_fh join "\t", $chr, $pos,
+              @{ $snps{$pos}{$par_id}{snp_del} }[ 0, 1 ], $par_id, "NA";
         }
     }
 }
@@ -122,16 +125,17 @@ exit;
 
 sub snp_hash_builder {
     my ( $par_id, $par_snps_file, $snps_ref ) = @_;
+
     open my $par_snps_fh, "<", $par_snps_file;
-    my $header = <$par_snps_fh>;    # do I care about this?
-    # my %par_snps;
+    <$par_snps_fh>;
     while (<$par_snps_fh>) {
         chomp;
         my @par_snp = split /,/;
         my @par_pos = split /\./, $par_snp[1];
         if ( defined $par_pos[1] ) {
-            $$snps_ref{$par_pos[0]}{$par_id}{$par_pos[1]} = [ @par_snp[ 2, 8 ], $par_pos[1] ];
-            $$snps_ref{$par_pos[0]}{$par_id}{insert}      = $par_pos[1];
+            $$snps_ref{ $par_pos[0] }{$par_id}{ $par_pos[1] } =
+              [ @par_snp[ 2, 8 ], $par_pos[1] ];
+            $$snps_ref{ $par_pos[0] }{$par_id}{insert} = $par_pos[1];
         }
         else {
             $$snps_ref{ $par_pos[0] }{$par_id}{snp_del} = [ @par_snp[ 2, 8 ] ];
@@ -145,7 +149,6 @@ sub get_cov {
 
     my $rs = $$schema_ref->resultset('Coverage')->search(
         {
-            # 'sample_id'  => $sample_id,
             'chromosome' => $chr,
             'position'   => $pos,
         },
