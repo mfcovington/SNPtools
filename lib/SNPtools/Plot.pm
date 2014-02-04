d0f289b00469027622ec46f5cea76ac27f8297c5
Mike Covington <mfcovington@gmail.com>
2014-02-04 15:55:48 -0800

    Extract private methods to SNPtools.pm

--- lib/SNPtools/Plot.pm
+++ lib/SNPtools/Plot.pm
@@ -3,7 +3,6 @@ use namespace::autoclean;
 use Moose;
 extends 'SNPtools';
 use MooseX::UndefTolerant;
-use File::Basename;
 use File::Path 'make_path';
 use Parallel::ForkManager;
 use autodie;
@@ -246,14 +245,6 @@ sub _plot_path {
     return $path;
 }
 
-sub _make_dir {
-    my $self = shift;
-
-    my ( $filename, $dir_name ) = fileparse( $self->out_file );
-    make_path( $dir_name );
-}
-
-
 sub _validity_tests {
     my $self = shift;
 
@@ -261,85 +252,6 @@ sub _validity_tests {
     # $self->_valid_fasta;
     $self->_valid_bam;
     # $self->_valid_bam_index;
-}
-
-sub _validity_tests_samtools {
-    my $self = shift;
-
-    $self->_valid_samtools_path;
-    $self->_valid_samtools_version;
-}
-
-sub _get_header {
-    my $self = shift;
-
-    $self->_validity_tests();
-    my $get_header_cmd = "samtools view -H " . $self->bam;
-    my @header = `$get_header_cmd`;
-    return @header;
-}
-
-sub _valid_bam {
-    my $self = shift;
-
-    if ( -e $self->bam and $self->bam =~ m/ \.bam$ /ix ) {
-        say "  Found valid bam file: " . $self->bam if $self->verbose;
-        return 1;
-    }
-    else {
-        die "  Can't find valid bam file: " . $self->bam;
-    }
-}
-
-sub _valid_bam_index {
-    my $self = shift;
-
-    my ( $bam_prefix, $bam_dir ) = fileparse( $self->bam, ".bam" );
-    if ( -e "$bam_dir/$bam_prefix.bai" or -e "$bam_dir/$bam_prefix.bam.bai" ) {
-        say "  Found valid index for " . $self->bam if $self->verbose;
-        return 1;
-    }
-    else {
-        say "  Can't find valid index for " . $self->bam;
-        $self->bam_index;
-    }
-}
-
-# sub _valid_fasta {
-#     my $self = shift;
-
-#     if ( -e $self->fasta and $self->fasta =~ m/ \.fasta$ | \.fa$ /ix ) {
-#         say "  Found valid fasta file: " . $self->fasta if $self->verbose;
-#         return 1;
-#     }
-#     else {
-#         die "  Can't find valid fasta file: " . $self->fasta;
-#     }
-# }
-
-sub _valid_samtools_path {
-    my $self = shift;
-
-    my $sam_status = `which samtools`;
-    die "  Samtools not found in PATH" unless $sam_status =~ m/samtools/i;
-    say "  Samtools looks good!" if $self->verbose;
-}
-
-sub _valid_samtools_version {
-    my $self = shift;
-
-    my @usage = `samtools 2>&1`; #change to samtools
-    my $version;
-    for (@usage) {
-        $_ =~ m/Version: ([\d\.].*) /i;
-        $version = $1 and last if $1;
-    }
-    my @version_parsed = $version =~ m/ (\d*) \. (\d*) \. (\d*) /x;
-    say "  Samtools is version $version" if $self->verbose;
-    die "  Need samtools version 0.1.XX+"
-      unless ( $version_parsed[0] >= 1
-        or $version_parsed[1] >= 2
-        or $version_parsed[1] == 1 and $version_parsed[2] >= 18 );
 }
 
 __PACKAGE__->meta->make_immutable;
--- lib/SNPtools/SNPfinder.pm
+++ lib/SNPtools/SNPfinder.pm
@@ -4,8 +4,6 @@ use Moose;
 extends 'SNPtools';
 use MooseX::UndefTolerant;
 use Modern::Perl;
-use File::Basename;
-use File::Path 'make_path';
 use Parallel::ForkManager;
 use autodie;
 # use Data::Printer;
@@ -136,14 +134,6 @@ has 'indel_min' => (
     lazy    => 1,
 );
 
-sub _make_dir {
-    my $self = shift;
-
-    my ( $filename, $dir_name ) = fileparse( $self->out_file );
-    make_path( $dir_name );
-}
-
-
 sub _validity_tests {
     my $self = shift;
 
@@ -151,85 +141,6 @@ sub _validity_tests {
     $self->_valid_fasta;
     $self->_valid_bam;
     $self->_valid_bam_index;
-}
-
-sub _validity_tests_samtools {
-    my $self = shift;
-
-    $self->_valid_samtools_path;
-    $self->_valid_samtools_version;
-}
-
-sub _get_header {
-    my $self = shift;
-
-    $self->_validity_tests();
-    my $get_header_cmd = "samtools view -H " . $self->bam;
-    my @header = `$get_header_cmd`;
-    return @header;
-}
-
-sub _valid_bam {
-    my $self = shift;
-
-    if ( -e $self->bam and $self->bam =~ m/ \.bam$ /ix ) {
-        say "  Found valid bam file: " . $self->bam if $self->verbose;
-        return 1;
-    }
-    else {
-        die "  Can't find valid bam file: " . $self->bam;
-    }
-}
-
-sub _valid_bam_index {
-    my $self = shift;
-
-    my ( $bam_prefix, $bam_dir ) = fileparse( $self->bam, ".bam" );
-    if ( -e "$bam_dir/$bam_prefix.bai" or -e "$bam_dir/$bam_prefix.bam.bai" ) {
-        say "  Found valid index for " . $self->bam if $self->verbose;
-        return 1;
-    }
-    else {
-        say "  Can't find valid index for " . $self->bam;
-        $self->bam_index;
-    }
-}
-
-sub _valid_fasta {
-    my $self = shift;
-
-    if ( -e $self->fasta and $self->fasta =~ m/ \.fasta$ | \.fa$ /ix ) {
-        say "  Found valid fasta file: " . $self->fasta if $self->verbose;
-        return 1;
-    }
-    else {
-        die "  Can't find valid fasta file: " . $self->fasta;
-    }
-}
-
-sub _valid_samtools_path {
-    my $self = shift;
-
-    my $sam_status = `which samtools`;
-    die "  Samtools not found in PATH" unless $sam_status =~ m/samtools/i;
-    say "  Samtools looks good!" if $self->verbose;
-}
-
-sub _valid_samtools_version {
-    my $self = shift;
-
-    my @usage = `samtools 2>&1`; #change to samtools
-    my $version;
-    for (@usage) {
-        $_ =~ m/Version: ([\d\.].*) /i;
-        $version = $1 and last if $1;
-    }
-    my @version_parsed = $version =~ m/ (\d*) \. (\d*) \. (\d*) /x;
-    say "  Samtools is version $version" if $self->verbose;
-    die "  Need samtools version 0.1.XX+"
-      unless ( $version_parsed[0] >= 1
-        or $version_parsed[1] >= 2
-        or $version_parsed[1] == 1 and $version_parsed[2] >= 18 );
 }
 
 __PACKAGE__->meta->make_immutable;
