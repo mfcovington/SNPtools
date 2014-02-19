910131b8b1e665b38847599a6cfdab4632a90827
Mike Covington <mfcovington@gmail.com>
2014-02-18 21:47:17 -0800

    Refactor genotype sub

--- bin/Genotype/genotyping_pileups.pl
+++ bin/Genotype/genotyping_pileups.pl
@@ -94,98 +94,111 @@ sub genotype {
         # - since that is the case, test to see how much overhead this check uses
         next unless exists $$snps{$position};
 
+        my $ref_base = ${ $$snps{$position} }[0]->{'ref_base'};
+        my $snp_base = ${ $$snps{$position} }[0]->{'snp_base'};
+
         # skip indels if ignoring indels
         next
-          if $no_indels
-          && ( ${ $$snps{$position} }[0]->{'ref_base'} eq 'INS'
-            || ${ $$snps{$position} }[0]->{'snp_base'} eq 'del' );
+            if $no_indels
+            && ( $ref_base eq 'INS' || $snp_base eq 'del' );
+
+        my $mpileup = $$mpileups{$position}->{'mpileup'};
 
         # remove "junk" characters from mpileups:
         # < and > are CIGAR Ns (or reference skips)
         # ^ and $ denote that the base is at the start or end of a read
         # the character after ^ indicates mapping quality for the read
-        $$mpileups{$position}->{'mpileup'} =~ s/ < | > | \^.{1} | \$ //gx;
+        $mpileup =~ s/<|>|\^.{1}|\$//g;
 
         # remove mpileups with deletions marked with a - sign.  They make no sense when I look at IGV, etc
-        $$mpileups{$position}->{'mpileup'} = "" if $$mpileups{$position}->{'mpileup'} =~ m/ [\.,] - \d+ [ACGT]+ /gix;
+        $mpileup = "" if $mpileup =~ m/[\.,]-\d+[ACGT]+/gi;
 
         # collect insertions
         my @insertions;
-        push ( @insertions, $1 ) while $$mpileups{$position}->{'mpileup'} =~ m/ [\.,] \+ \d+ ([ACGT]+) /gix;
+        push @insertions, $1 while $mpileup =~ m/[\.,]\+\d+([ACGT]+)/gi;
 
         # remove insertions
-        $$mpileups{$position}->{'mpileup'} =~ s/ [\.,] \+ \d+ [ACGT]+ //gix;
+        $mpileup =~ s/[\.,]\+\d+[ACGT]+//gi;
 
         # convert , and . to ref_base
-        $$mpileups{$position}->{'mpileup'} =~ s/ \. | , /$$mpileups{$position}->{'ref_base'}/gix;
+        $mpileup =~ s/\.|,/$$mpileups{$position}->{'ref_base'}/gi;
+
+        my $insert_pos = ${ $$snps{$position} }[0]->{'insert_pos'};
 
         # at least for now, skip polymorphisms where
         # one parent is a SNP/del and the other is an insertion
         # (very rare and not labeled as DIFF_SNPs)
         next
-          if scalar @{ $$snps{$position} } > 1
-          && ${ $$snps{$position} }[0]->{'insert_pos'} eq 'NA';
+            if scalar @{ $$snps{$position} } > 1
+            && $insert_pos eq 'NA';
 
         # skip positions with problematic artifact created by removing "NOT" polymorphisms
         # that were actually "DIFF_SNPS" with inserts of varying size
         next
-          if looks_like_number( ${ $$snps{$position} }[0]->{'insert_pos'} )
-          && ${ $$snps{$position} }[0]->{'insert_pos'} > 1;
+            if looks_like_number($insert_pos)
+            && $insert_pos > 1;
 
         # separate snp classes and determine genotype of parents from snp hash
-        my (
-            $par1_base,     $par2_base, @insert_bases,
+        my ($par1_base,     $par2_base, @insert_bases,
             $insert_parent, $is_insert, $insert_seq,
             $del_parent,    $is_del,    $skip
         );
 
+        my $genotype = ${ $$snps{$position} }[0]->{'genotype'};
+
         if ( ${ $$snps{$position} }[0]->{'snp_class'} eq 'DIFF_SNP' ) {
-            $par1_base = ${ $$snps{$position} }[0]->{'snp_base'}
-              if ${ $$snps{$position} }[0]->{'genotype'} eq $par1_id;
-            $par2_base = ${ $$snps{$position} }[0]->{'snp_base'}
-              if ${ $$snps{$position} }[0]->{'genotype'} eq $par2_id;
+            $par1_base = $snp_base
+                if $genotype eq $par1_id;
+            $par2_base = $snp_base
+                if $genotype eq $par2_id;
         }
-        elsif ( ${ $$snps{$position} }[0]->{'ref_base'} eq 'INS' ) {
+        elsif ( $ref_base eq 'INS' ) {
             $is_insert     = 1;
-            $insert_parent = ${ $$snps{$position} }[0]->{'genotype'};
+            $insert_parent = $genotype;
             push( @insert_bases, ${ $$snps{$position} }[$_]->{'snp_base'} )
-              for ( 0 .. $#{ $$snps{$position} } );
+                for ( 0 .. $#{ $$snps{$position} } );
         }
-        elsif ( ${ $$snps{$position} }[0]->{'snp_base'} eq 'del' ) {
+        elsif ( $snp_base eq 'del' ) {
             $is_del     = 1;
-            $del_parent = ${ $$snps{$position} }[0]->{'genotype'};
-            $par1_base  = ${ $$snps{$position} }[0]->{'ref_base'}
-              if $del_parent eq $par2_id;
-            $par2_base = ${ $$snps{$position} }[0]->{'ref_base'}
-              if $del_parent eq $par1_id;
+            $del_parent = $genotype;
+            $par1_base  = $ref_base
+                if $del_parent eq $par2_id;
+            $par2_base = $ref_base
+                if $del_parent eq $par1_id;
         }
-        elsif ( ${ $$snps{$position} }[0]->{'genotype'} eq $par1_id ) {
-            $par1_base = ${ $$snps{$position} }[0]->{'snp_base'};
-            $par2_base = ${ $$snps{$position} }[0]->{'ref_base'};
+        elsif ( $genotype eq $par1_id ) {
+            $par1_base = $snp_base;
+            $par2_base = $ref_base;
         }
-        elsif ( ${ $$snps{$position} }[0]->{'genotype'} eq $par2_id ) {
-            $par1_base = ${ $$snps{$position} }[0]->{'ref_base'};
-            $par2_base = ${ $$snps{$position} }[0]->{'snp_base'};
+        elsif ( $genotype eq $par2_id ) {
+            $par1_base = $ref_base;
+            $par2_base = $snp_base;
         }
         else { die "SOMETHING IS WRONG!!!"; }
 
         # count stuff up
-        my $A_count   = $$mpileups{$position}->{'mpileup'} =~ tr/Aa//;
-        my $C_count   = $$mpileups{$position}->{'mpileup'} =~ tr/Cc//;
-        my $T_count   = $$mpileups{$position}->{'mpileup'} =~ tr/Tt//;
-        my $G_count   = $$mpileups{$position}->{'mpileup'} =~ tr/Gg//;
-        my $del_count = $$mpileups{$position}->{'mpileup'} =~ tr/*//;
+        my $A_count   = $mpileup =~ tr/Aa//;
+        my $C_count   = $mpileup =~ tr/Cc//;
+        my $T_count   = $mpileup =~ tr/Tt//;
+        my $G_count   = $mpileup =~ tr/Gg//;
+        my $del_count = $mpileup =~ tr/*//;
         my $insert_count = scalar @insertions;
-        my $total_count =
-          $A_count + $C_count + $T_count + $G_count + $del_count + $insert_count;
+        my $total_count
+            = $A_count
+            + $C_count
+            + $T_count
+            + $G_count
+            + $del_count
+            + $insert_count;
         my $par1_count = 0;
         my $par2_count = 0;
 
         if ($is_insert) {
             $insert_seq = join "", @insert_bases;
             my $mpileup_inserts = join " ", @insertions;
-            my $matched_insert_count =
-              $mpileup_inserts =~ s/\b$insert_seq\b/\b$insert_seq\b/gi; # added word boundaries so AA wouldn't match AAAA twice, for example
+            my $matched_insert_count
+                = $mpileup_inserts =~ s/\b$insert_seq\b/\b$insert_seq\b/gi
+                ; # added word boundaries so AA wouldn't match AAAA twice, for example
             if ( $insert_parent eq $par1_id ) {
                 $par1_count = $matched_insert_count;
                 $par2_count = max( $A_count, $C_count, $T_count, $G_count );
@@ -199,29 +212,28 @@ sub genotype {
         elsif ($is_del) {
             if ( $del_parent eq $par1_id ) {
                 $par1_count = $del_count;
-                $par2_count =
-                  $$mpileups{$position}->{'mpileup'} =~ s/$par2_base/$par2_base/gi;
+                $par2_count = $mpileup =~ s/$par2_base/$par2_base/gi;
             }
             elsif ( $del_parent eq $par2_id ) {
-                $par1_count =
-                  $$mpileups{$position}->{'mpileup'} =~ s/$par1_base/$par1_base/gi;
+                $par1_count = $mpileup =~ s/$par1_base/$par1_base/gi;
                 $par2_count = $del_count;
             }
             else { die "SOMETHING IS WRONG!!!"; }
         }
         else {
-            $par1_count =
-              $$mpileups{$position}->{'mpileup'} =~ s/$par1_base/$par1_base/gi
-              if defined $par1_base;   # reminder: parent base undefined for inserts
-            $par2_count =
-              $$mpileups{$position}->{'mpileup'} =~ s/$par2_base/$par2_base/gi
-              if defined $par2_base;   # reminder: parent base undefined for inserts
+            # reminder: parent base undefined for inserts
+            $par1_count = $mpileup =~ s/$par1_base/$par1_base/gi
+                if defined $par1_base;
+            $par2_count = $mpileup =~ s/$par2_base/$par2_base/gi
+                if defined $par2_base;
         }
 
         $par1_count = 0 unless $par1_count;
         $par2_count = 0 unless $par2_count;
 
-        say $out_fh join( "\t", $chromosome, $position, $par1_count, $par2_count, $total_count );
+        say $out_fh join( "\t",
+            $chromosome, $position, $par1_count,
+            $par2_count, $total_count );
 
     }
 
